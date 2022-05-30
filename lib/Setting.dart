51f0992dd7a4d90e62ac1ad9947c8e3428a13700
willzed <wiliamwijaya101@gmail.com>
2022-05-30 17:24:49 +0700

    commit

--- lib/Setting.dart
+++ lib/Setting.dart
@@ -161,7 +161,7 @@ class _SettingsState extends State<Settings> {
                                   padding:
                                       EdgeInsets.only(left: 10, bottom: 2)),
                               Text(
-                                  'Atur Display Picture, Username dan Nomor Telepon',
+                                  'Atur Akun, Username, Email dan Nomor Telepon',
                                   style: TextStyle(color: Colors.white)),
                               Padding(padding: EdgeInsets.all(2)),
                             ],
